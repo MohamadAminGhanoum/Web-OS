48b19c5b2e2a5cf9f6b66e506b5ed7dcc9d034e2
MohamadAminGhanoum <mohamad.amin.ghanoum@gmail.com>
2026-03-02 22:06:23 +0100

    Added battery indicator percentage

--- script.js
+++ script.js
@@ -7,6 +7,12 @@
     updateTime();
     setInterval(updateTime, 1000);
 
+const BatteryLevel = document.querySelector(".battery");
+navigator.getBattery().then(function(battery) {
+    const level = battery.level;
+    const status = level * 100 + "%";
+    BatteryLevel.innerHTML = status;
+});
 
 
 
